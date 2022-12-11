c974f773f8499f97b51c311d2a7d2292b2956007
Alidonis <64419217+Alidonis@users.noreply.github.com>
2022-12-11 12:30:53 +0100

    Ajoutez des fichiers projet.

---
+++ ConsoleApp1/MainClass.cs
@@ -0,0 +1,37 @@
+﻿using ConsoleApp1;
+using System;
+
+namespace ConsoleApp1
+{
+    internal class MainClass
+    {
+        private bool ShouldRun = true;
+        public MainClass() {
+            ReadUserInput();
+        }
+        public void ReadUserInput()
+        {
+            while (ShouldRun)
+            {
+                Console.Write("> ");
+                var input = Console.ReadLine();
+                HandleUserCommand(input);
+            }
+        }
+        void HandleUserCommand(string? input)
+        {
+            if (input == "") { return; }
+            switch (input)
+            {
+                case "local pvp":
+                    break;
+                case "exit":
+                    ShouldRun = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
+
---
+++ ConsoleApp1/pawns.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    struct ChessPawnPosition
+    {
+        public int x;
+        public int y;
+    }
+    struct ChessGridInfo
+    {
+        public int length;
+        public int height;
+        public ChessPawn[,] Grid;
+        public ChessGridInfo(ChessPawn[,] _Grid)
+        {
+            Grid= _Grid;
+        }
+    }
+    internal class ChessPawn
+    {
+        internal char Icon = '!';
+        internal ChessPawnPosition Position;
+        internal ChessPawn(char _icon, ChessPawnPosition _startingPos) 
+        {
+            Icon = _icon;
+            Position = _startingPos;
+        }
+        internal virtual void move(int x, int y, int boardLength, int boardHeight)
+        {
+            
+        }
+    }
+}
