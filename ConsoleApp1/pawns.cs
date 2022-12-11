4286d1bac97f70c108041ff99c74b36c31e9abb1
Alidonis <64419217+Alidonis@users.noreply.github.com>
2022-12-11 12:32:15 +0100

    Create README.md

--- ConsoleApp1/pawns.cs
+++ ConsoleApp1/pawns.cs
@@ -17,9 +17,24 @@ namespace ConsoleApp1
             Grid= _Grid;
         }
     }
+    enum ChessPieceRank
+    {
+        Rank0 = '\u2080',
+        Rank1 = '\u2081',
+        Rank2 = '\u2082',
+        Rank3 = '\u2083',
+        Rank4 = '\u2084',
+        Rank5 = '\u2085',
+        Rank6 = '\u2086',
+        Rank7 = '\u2087',
+        Rank8 = '\u2088',
+        Rank9 = '\u2089',
+
+    }
     internal class ChessPawn
     {
         internal char Icon = '!';
+        internal char Rank = '\u2080';
         internal ChessPawnPosition Position;
         internal ChessPawn(char _icon, ChessPawnPosition _startingPos) 
         {
